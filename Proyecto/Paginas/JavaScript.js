6c4440e421992af1882038827d45be6378c33183
Mauricio Melendez <mmelendez100404@gmail.com>
2020-11-23 20:09:04 -0600

    Formulario en login

--- Proyecto/Paginas/JavaScript.js
+++ Proyecto/Paginas/JavaScript.js
@@ -1,3 +1,8 @@
 function login(){
-    window
+    document.getElementById("drop-content").style.display = "block"
+}
+function info_login(nombre){
+    nombre = document.getElementById("nombre_usuario").value;
+    correo = document.getElementById("correo_usuario").value;
+    document.getElementById("boton").innerHTML = nombre
 }
