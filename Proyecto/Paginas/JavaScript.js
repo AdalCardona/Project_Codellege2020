7e8c999f810a86a69146319ba3a83d5d73562fcb
Mauricio Melendez <mmelendez100404@gmail.com>
2020-11-24 18:31:00 -0600

    Correccion de errores

--- Proyecto/Paginas/JavaScript.js
+++ Proyecto/Paginas/JavaScript.js
@@ -1,8 +1,8 @@
 function login(){
     document.getElementById("drop-content").style.display = "block"
 }
-function info_login(nombre){
-    nombre = document.getElementById("nombre_usuario").value;
-    correo = document.getElementById("correo_usuario").value;
-    document.getElementById("boton").innerHTML = nombre
+function info_login(){
+    document.getElementById("drop-content").style.display = "none"
+    nombre = document.getElementById("usuario").value;
+    contraseña = document.getElementById("contraseña").value;
 }
