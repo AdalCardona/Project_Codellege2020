8283e75aa8232e80056b5173565056b018347293
Mauricio Melendez <mmelendez100404@gmail.com>
2020-11-25 18:54:21 -0600

    Cambio en noticias de covid

--- Proyecto/Paginas/JavaScript.js
+++ Proyecto/Paginas/JavaScript.js
@@ -2,7 +2,9 @@ function login(){
     document.getElementById("drop-content").style.display = "block"
 }
 function info_login(){
-    document.getElementById("drop-content").style.display = "none"
     nombre = document.getElementById("usuario").value;
     contraseña = document.getElementById("contraseña").value;
+}
+function atras(){
+    document.getElementById("drop-content").style.display = "none"
 }
