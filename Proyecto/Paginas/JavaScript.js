92f38a89efa8afc78edada7ae074d193ebb2f4fa
Mauricio Melendez <mmelendez100404@gmail.com>
2020-11-26 16:16:56 -0600

    Cambios de info. de covid

--- Proyecto/Paginas/JavaScript.js
+++ Proyecto/Paginas/JavaScript.js
@@ -1,10 +1,19 @@
 function login(){
-    document.getElementById("drop-content").style.display = "block"
+    document.getElementById("drop-content").style.display = "block";
 }
 function info_login(){
     nombre = document.getElementById("usuario").value;
     contraseña = document.getElementById("contraseña").value;
+    if (nombre == "Alex09"  && contraseña == "code123"){
+        x = document.getElementById("boton1").value = "Alex09";
+        document.getElementById("boton1").style.background = "grey";
+        document.getElementById("boton1").style.padding = "5px";
+        document.getElementById("drop-content").style.display = "none";
+    }
+    else{
+        alert("Usuario o contraseña incorrecto");
+    }
 }
 function atras(){
-    document.getElementById("drop-content").style.display = "none"
+    document.getElementById("drop-content").style.display = "none";
 }
