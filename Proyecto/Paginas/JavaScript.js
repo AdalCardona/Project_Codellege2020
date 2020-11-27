6d4eb7366859491d9ac23c32a8258688d3336e13
Mauricio Melendez <mmelendez100404@gmail.com>
2020-11-26 18:20:48 -0600

    Cambios en login

--- Proyecto/Paginas/JavaScript.js
+++ Proyecto/Paginas/JavaScript.js
@@ -5,10 +5,11 @@ function info_login(){
     nombre = document.getElementById("usuario").value;
     contraseña = document.getElementById("contraseña").value;
     if (nombre == "Alex09"  && contraseña == "code123"){
-        x = document.getElementById("boton1").value = "Alex09";
         document.getElementById("boton1").style.background = "grey";
         document.getElementById("boton1").style.padding = "5px";
+        document.getElementById("boton1").style.display = "none";
         document.getElementById("drop-content").style.display = "none";
+        document.getElementById("nombre_usuario").innerHTML = "Alex09";
     }
     else{
         alert("Usuario o contraseña incorrecto");
